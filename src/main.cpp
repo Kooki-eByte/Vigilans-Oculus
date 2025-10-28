6e301ee3fac47c31c45050a9aa7deb07cfc9e8b2
Cristian Hornedo <crisitanhornedo@gmail.com>
2025-10-28 19:05:30 -0400

    Start building up the instance and code to get vulkan started

--- src/main.cpp
+++ src/main.cpp
@@ -1,71 +1,143 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-#define GLM_FORCE_RADIANS
-#define GLM_FORCE_DEPTH_ZERO_TO_ONE
-#include <glm/vec4.hpp>
-#include <glm/mat4x4.hpp>
+// #define GLM_FORCE_RADIANS
+// #define GLM_FORCE_DEPTH_ZERO_TO_ONE
+// #include <glm/vec4.hpp>
+// #include <glm/mat4x4.hpp>
 
 #include <stdio.h>
+#include <stdbool.h>
+#include <cstdlib>
 #include "defines.h"
+#define GREED_IMPLEMENTATION
+#include "../include/greed/greed.h"
 
 // Following a Vulkan tutorial guide to draw a triangle:
 // https://vulkan-tutorial.com/resources/vulkan_tutorial_en.pdf
 
-// Steps to draw a triangle with the Verbose API of vulkan.
+// TODO: Remove these globals into a better location later
+static VkInstance instance;
 
-// Create a VkInstance
-// Select a supported graphics card (VkPhysicalDevice)
-// Create a VkDevice and VkQueue for drawing and presentation
-// Create a window, window surface and swap chain
-// Wrap the swap chain images into VkImageView
-// Create a render pass that specifies the render targets and usage
-// Create framebuffers for the render pass
-// Set up the graphics pipeline
-// Allocate and record a command buffer with the draw commands for every possible swap chain image
-// Draw frames by acquiring images, submitting the right draw command buffer and returning the images back to the swap chain
+typedef struct window_settings {
+  int width;
+  int height;
+  const char *title;
+} window_settings;
 
-// Step 1: Instance and Physical device selection
+bool createInstance() {
+  VkApplicationInfo appInfo = {};
+  appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+  appInfo.pApplicationName = "Vigilans Oculus";
+  appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.pEngineName = "Eureka";
+  appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+  appInfo.apiVersion = VK_API_VERSION_1_0;
 
-// Step 2: Logical device and queue families 
+  VkInstanceCreateInfo createInfo = {};
+  createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  createInfo.pApplicationInfo = &appInfo;
 
-// Step 3: Window surface and swap chain
+  u32 glfwExtensionCount = 0;
+  const char **glfwExtensions;
 
-// Step 4: Image views and framebuffers
+  u32 extCountForVulkan = 0;
+  vkEnumerateInstanceExtensionProperties(0, &extCountForVulkan, 0);
 
-// step 5: Render passes
+  VkExtensionProperties extensions[extCountForVulkan] = {};
+  vkEnumerateInstanceExtensionProperties(0, &extCountForVulkan, extensions);
 
-// Step 6: Graphics pipeline
+  fprintf(stderr, "Available extensions:");
+  for (u32 i = 0; i < extCountForVulkan; i++) {
+    fprintf(stderr, "\t%s\n", extensions->extensionName);
+  }
 
-// Step 7: Command pools and command buffers
+  // Useful glfw built-in function to get the required extensions for GLFW to work.
+  glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
 
-// Step 8: Main loop
-int main(void) {
+  createInfo.enabledExtensionCount = glfwExtensionCount;
+  createInfo.ppEnabledExtensionNames = glfwExtensions;
+
+  // Last two attributes are for validation layer
+  createInfo.enabledLayerCount = 0;
+
+  // TODO: 2nd arg will be for arena allocator later on.
+  if (vkCreateInstance(&createInfo, 0, &instance) != VK_SUCCESS) {
+    g_log_error("Failed to create vulkan instance!");
+    return false;
+  }
+
+  return true;
+}
+
+bool initWindow(GLFWwindow *win, window_settings *ws) {
   glfwInit();
-
   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  GLFWwindow *window = glfwCreateWindow(1280, 720, "Vigilans Oculus", 0, 0);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+  
+  win = glfwCreateWindow(ws->width, ws->height, ws->title, 0, 0);
 
-  u32 extenstionCount = 0;
-  vkEnumerateInstanceExtensionProperties(0, &extenstionCount, 0);
+  if (win == NULL) {
+    g_log_error("Window is null, after trying to create window!");
+    return false;
+  }
 
-  printf("%d extensions supported\n", extenstionCount);
+  return true;
+}
 
-  glm::mat4 matrix = {};
-  glm::vec4 vec = {};
-  auto test = matrix * vec;
+bool initVulkan() {
+  if (!createInstance()) {
+    g_log_error("Faield to initialize Vulkan!");
+    return false;
+  } 
+  return true;
+}
 
-  while(!glfwWindowShouldClose(window)) {
+void mainLoop(GLFWwindow *win) {
+  while (!glfwWindowShouldClose(win)) {
     glfwPollEvents();
   }
+}
 
-  for(int i = 0; i < 4; i++) {
-    printf("%f\n", test[i]);
+void cleanup(GLFWwindow *win) {
+  // TODO: Ensure you change the 2nd arg to the free arena allocator 
+  vkDestroyInstance(instance, 0);
+
+  glfwDestroyWindow(win);
+  glfwTerminate();
+}
+
+bool runApp(int width, int height, const char *window_name) {
+  GLFWwindow *window = {};
+  
+  window_settings settings = {
+    .width = width,
+    .height = height,
+    .title = window_name
+  };
+
+  if (!initWindow(window, &settings)) {
+    g_log_error("The GLFW Window failed to initialize!");
+    return false;
   }
 
-  glfwDestroyWindow(window);
+  if (!initVulkan()) {
+    g_log_error("Failed to initialize Vulkan!");
+    return false;
+  }
 
-  glfwTerminate();
+  mainLoop(window);
 
-  return 0;
+  cleanup(window);
+
+  return true;
+}
+
+int main(void) {
+  if (!runApp(1280, 720, "Vigilans Oculus")) {
+    g_log_error("App failed to run!");
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
